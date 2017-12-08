72cce7925cdf1042048078c530b602bda06963ae
Don Rivard <drivard@demandware.com>
2017-12-08 15:36:42 -0500

    passed step 4. All code and events

--- deploy/aura/BoatSearchResults/BoatSearchResultsController.js
+++ deploy/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -1,6 +1,15 @@
 ({
     doSearch  : function(component, event, helper) {
 			  helper.onSearch(component, event, helper);
-    }
+    },
+    search: function(component, event, helper){
+        console.log("BSRController: search called");
+        var params = event.getParam('arguments');
+        console.log("boatTypeId extracted: " + params.boatTypeId);
+        component.set("v.typeId", params.boatTypeId);
+       
+        helper.onSearch(component);
+        return "search complete.";
+    },
 
 })
