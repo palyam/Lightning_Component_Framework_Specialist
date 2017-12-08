28c029e0c12e28775a733623248bd1bd54d0cc1e
Don Rivard <drivard@demandware.com>
2017-12-08 13:31:25 -0500

    added new code.

--- force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
+++ force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -1,6 +1,12 @@
 ({
     doSearch  : function(component, event, helper) {
 			  helper.onSearch(component, event, helper);
+    },
+
+    onBoatSelect : function(component, event, helper) {
+        var boatId = event.getParam("boatId");
+        component.set("v.selectedBoatId",boatId);
+
     }
 
 })
